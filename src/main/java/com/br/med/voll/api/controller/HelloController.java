80cb5094bb1c0f66227c2286f149291c06556ab5
RafaelRenaud <renaudrafa@hotmail.com>
2023-03-19 21:42:58 -0300

    conf: Initital API Configuration

---
+++ src/main/java/com/br/med/voll/api/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.br.med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String status(){
+        return "'status':'active'";
+    }
+}
